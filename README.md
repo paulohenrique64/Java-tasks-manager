64a03c6f17e15ae9ee2af7fd82c0c85c3bd600cd
Paulo Henrique <paulohenriquelvs20@gmail.com>
2024-06-26 11:02:32 -0300

    created Makefile and added a command to create a project jar file

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 * sqlite
 * picocli
+
--- src/main/resources/DriverDatabase.java
+++ src/main/resources/DriverDatabase.java
@@ -3,13 +3,22 @@ package main.resources;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.File;
 
 public class DriverDatabase {
     public static Connection getConnection() {
         Connection conn = null;
 
         try {
-            String url = "jdbc:sqlite:C:tasks-database.db";
+            String homePath = System.getProperty("user.home") + File.separator;
+            File theDir = new File(homePath + ".mytasks");
+
+            if (!theDir.exists()) {
+                theDir.mkdirs();
+            }
+
+            String url = "jdbc:sqlite:" + homePath + ".mytasks" + File.separator + "database.db";
+
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
